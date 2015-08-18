99e232f8200e317fdb69b91ac10b16d0e92c5ce5
chyyuu <chyyuu@gmail.com>
2015-08-18 14:23:55 +0800

    update build-eglibc.sh for ubuntu compiling. But still error in compiling eglibc

--- build-eglibc.sh
+++ build-eglibc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # TODO: Check that svn, patch, and make are installed on the system
-
+#setenv CFLAGS "-U_FORTIFY_SOURCE -O2 -fno-stack-protector"
+export CFLAGS="-U_FORTIFY_SOURCE -O2 -fno-stack-protector"
 svn co svn://svn.eglibc.org/branches/eglibc-2_14 eglibc-2.14
 cd eglibc-2.14/libc
 cat ../../eglibc-2.14.diff | patch -p2 -d ./
